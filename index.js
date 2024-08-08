80c5021aa9dd0e875fba2afa906a0524a40db099
Lisandro Luna <18648174+LisandroLuna@users.noreply.github.com>
2024-08-08 00:26:07 -0300

    add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// Configura el puerto en el que escuchará el servidor
+const PORT = process.env.PORT || 3000;
+
+// Crea el servidor
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, MundosE!\n');
+});
+
+// Inicia el servidor y escucha en el puerto configurado
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
