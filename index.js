7db118e26d9123bf1b5dba8326de3f5ca663949f
Lisandro Luna <18648174+LisandroLuna@users.noreply.github.com>
2024-08-08 21:39:41 -0300

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, MundosE!\n');
+  res.end('Hola, MundosE!\n');
 });
 
 // Inicia el servidor y escucha en el puerto configurado
